8d5df60211bc28a085712ae147ef465bad2321f8
LauRadulea <112471183+LauRadulea@users.noreply.github.com>
2022-12-10 15:50:10 +0000

    Add files via upload

---
+++ rockPaperScissor/index.js
@@ -0,0 +1,47 @@
+let scorePlayer = 0;
+let scoreComputer = 0;
+const choices = ["rock", "paper", "scissor"];
+const resultsOutputText = document.querySelector('[data-results]')
+const selectionButtons = document.querySelectorAll('[data-selection]');
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener( 'click', () => {
+    const playerSelection = selectionButton.dataset.selection;
+    const computerSelection = computerPlay();
+    if(scorePlayer < 5 && scoreComputer <5) 
+      playRound(playerSelection, computerSelection);
+    else {
+      displayWinner(); 
+      scorePlayer = 0
+      scoreComputer = 0
+    }
+    })
+})
+
+function computerPlay () {
+    return choices [Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === computerSelection) {
+    resultsOutputText.innerText = `It's a draw! The score is: ${scoreComputer}:${scorePlayer}`
+  } else if (playerSelection === "rock" && computerSelection === "scissor" 
+          || playerSelection === "paper" && computerSelection === "rock" 
+          || playerSelection === "scissor" && computerSelection === "paper") {
+    scorePlayer++;
+    resultsOutputText.innerText = `You are the winner! The score is: ${scorePlayer}:${scoreComputer}`
+  } else {
+    scoreComputer++;
+    resultsOutputText.innerText = `Computer is the winner! The score is: ${scoreComputer}:${scorePlayer}`
+  }
+}
+
+function displayWinner() {
+  if(scoreComputer == scorePlayer) {
+    resultsOutputText.innerText = `It's a draw! The score is: ${scoreComputer}:${scorePlayer}`
+  } else if (scoreComputer > scorePlayer) {
+    resultsOutputText.innerText = `Computer is the winner! The score is: ${scoreComputer}:${scorePlayer}`
+  } else {
+    resultsOutputText.innerText = `You are the winner! The score is: ${scoreComputer}:${scorePlayer}`
+  }
+}
